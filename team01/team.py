6fd1b4cc11f84045350ea6fe91269ee564ef5fb5
y00nsy <pink0884@naver.com>
2021-11-19 10:16:23 +0900

    [21.11.19] 회원가입,프로그램종료

--- team01/team.py
+++ team01/team.py
@@ -1,7 +1,20 @@
 #전역
-id_list=[]  #회원가입하면 저장될 아이디 리스트
-pw_list=[]  #회원가입하면 저장될 비번 리스트
 
+# id_list=[]  #회원가입하면 저장될 아이디 리스트
+# pw_list=[]  #회원가입하면 저장될 비번 리스트
+
+user_list = [
+    {
+        "id": "abc123",
+        "pw": "1234!",
+        "name": "김철수"
+    },
+    {
+        "id": "def123",
+        "pw": "5432!",
+        "name": "홍길동"
+    }
+]
 
 
 #함수 정의부
@@ -13,19 +26,41 @@ def show_first():
     print('2. 기존 직원 로그인하기')
     print('3. 프로그램 종료하기')
 
-#중복을 확인하는 함수
+#중복을 확인하는 함수(아이디)
+def check_duplicate_code_id():
+    while True:
+
+        code = input('아이디 >> ')
+        flag = False  # 중복 플래그
+
+        # 제품번호 중복검증
+        for u in user_list:
+            if code == u['id']:
+
+                # 중복된 경우
+                print('\n# 아이디가 이미 존재합니다. 다시 입력하세요.')
+                flag = True
+                break
+        if flag == False:
+            return code     # 중복안된 제품번호
+
 
 # 신규직원등록 (회원가입)
-def insert_id():    
-    get_id = {}
+def insert_id_pw():    
+    
+    user = {}
     print('\n>>> 신규 직원 등록을 시작합니다.')
+    user['id'] = check_duplicate_code_id()
+    user['pw'] = input('비밀번호 >> ')
+    user['name'] = input('이름 >> ')
 
-
-
+    user_list.append(user)
     
+    print('\n회원가입이 완료되었습니다.')
 
-    
-    
+# 기존 직원 로그인
+
+
 
 #프로그램 종료처리하기
 def exit_program():
@@ -51,7 +86,7 @@ if __name__ == '__main__':
         
         menu = int(input('\n >>> '))
         if menu == 1:
-            pass
+            insert_id_pw()
         elif menu == 2:
             pass
         else:
