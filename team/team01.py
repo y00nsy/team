506926f6d7a05b0eb66ddfcf3608f8292820350e
y00nsy <pink0884@naver.com>
2021-11-24 08:54:54 +0900

    [21.11.23] 직원정보조회,삭제,수정 추가

--- team/team01.py
+++ team/team01.py
@@ -103,6 +103,7 @@ def load_book_store():
     global book_store    
 
     if not os.path.isdir(dir_name): return
+    if not os.path.isfile(dir_name+file_name2): return
 
     try:
         f = open(dir_name+file_name2, 'rb')
@@ -115,10 +116,25 @@ def load_book_store():
 
 #첫 회원가입/로그인 화면 보여주기
 def show_first():
-    print("\n\n====== 도서 재고관리 프로그램 ======")
+    print('\n\n=====================================')
+    print("=====> 도서 재고관리 프로그램 <=====")
+    print('='*37)
     print('1. 신규 직원 등록하기')
     print('2. 기존 직원 로그인하기')
-    print('3. 프로그램 종료하기')
+    print('3. 직원 정보 조회하기')
+    
+    print('4. 프로그램 종료하기')
+    print('='*37)
+
+# 회원정보 화면 출력
+def show_users_info():
+    print('\n\n==============================')
+    print('=====> 직원 정보 관리 <=====')
+    print('='*30)
+    print('1. 직원 정보 수정하기')
+    print('2. 직원 정보 삭제하기')
+    print('3. 처음 화면으로 돌아가기')
+    print('='*30)
 
 #중복을 확인하는 함수(아이디)
 def check_duplicate_code_id():
@@ -193,7 +209,7 @@ def login():
                             
         while True:
             if in_pw == real_pw:
-                print('>> {}님, 로그인에 성공하셨습니다. <<'.format(already_user['id']))
+                print('\n>> {}님, 로그인에 성공하셨습니다. <<'.format(already_user['id']))
                 return True
             else :
                 print('\n!!! 비밀번호를 잘못 입력했습니다 !!!')
@@ -204,6 +220,79 @@ def login():
                                 
     else:
         print('\n!!! 가입되지 않은 정보입니다 !!!')
+
+# 직원 정보 출력 머릿말 부분 함수
+def header_user_print():
+    print('\n\n=====================================')
+    print('\t*** 서점 직원 정보 ***')
+    print('=' * 37)
+    print('{:^10s}{:^12s}{:^14s}'.format('id','pw','name'))
+    print('=' * 37)
+
+# 기존 직원 정보 조회하는 함수
+def print_all_users():
+    header_user_print()
+
+    for users in user_list:
+        print('{:^10s}{:^14s}{:^10s}'.format(users['id'],users['pw'],users['name']))
+    
+    print('=' * 37)
+
+# 회원코드를 입력받는 함수
+def input_user_code(msg):
+    print(f'\n# {msg}하실 회원의 id를 입력하세요.')
+    code_id = input('>> ')
+    return code_id
+
+# 회원코드로 해당 회원정보를 찾아오는 함수
+def get_user_info(code_id):
+    for info in user_list:
+        if code_id == info['id']:
+            return info
+    return {}
+
+# 회원정보 수정 처리 함수
+def modify_info():
+    code_id = input_user_code('수정')
+    info = get_user_info(code_id)
+
+    if len(info) > 0:
+        print('\n# [ID: {}] {}님의 정보를 수정합니다.'.format(info['id'],info['name']))
+
+        print('[ 1. id 변경 | 2. pw 변경 | 3. name 변경 | 4. 취소 ]')
+        select = int(input('>> '))
+
+        if select == 1:
+            info['id'] = input('\n>> ID 수정(변경 전: {}) >> '.format(info['id']))
+            print('\n>> 정보수정이 정상 처리되었습니다. <<')
+
+        elif select == 2:
+            info['pw'] = input('\n>> PW 수정(변경 전: {}) >> '.format(info['pw']))
+            print('\n>> 정보수정이 정상 처리되었습니다. <<')
+        elif select == 3:
+            info['name'] = input('\n>> 이름 수정(변경 전: {}) >> '.format(info['name']))
+            print('\n>> 정보수정이 정상 처리되었습니다. <<')
+        else:
+            print('\n>> 변경을 취소합니다. <<')
+
+    else:
+        print('\n[ID : {}] 해당하는 직원 정보가 등록되지 않았습니다.'.format(code_id))
+    save_user_list()
+
+# 회원정보 삭제 처리 함수
+def delete_info():
+    code_id = input_user_code('삭제')
+    info = get_user_info(code_id)
+
+    if len(info) > 0:
+        user_list.remove(info)
+        print('\n>> {}님의 정보삭제가 정상 처리되었습니다. <<'.format(info['name']))
+
+    else:
+        print('\n[ID: {}]에 해당하는 직원 정보가 등록되지 않았습니다.'.format(code_id))
+    save_user_list()
+
+
            
 ######################################################################################
    
@@ -216,7 +305,8 @@ def show_second():
     print('# 3. 개별 도서 조회하기')
     print('# 4. 도서 정보 수정')
     print('# 5. 도서 정보 삭제')
-    print('# 6. 프로그램 종료')
+    print('# 6. 로그아웃')
+    print("=========================")
 
 # 도서 번호의 중복을 확인하는 함수
 def check_book_code():
@@ -228,7 +318,7 @@ def check_book_code():
 
         for b in book_store:
             if book_code == b['책번호']:
-                print('\n!!! 도서 번호가 중복되었습니다. 다시 입력하세요 !!!')
+                print('\n>>> 도서 번호가 중복되었습니다. 다시 입력하세요.')
                 flag = True
                 break
         if flag == False:
@@ -316,7 +406,7 @@ def modify_book():
             book['가격'] = int(input('=> 수정할 가격({}) >> '.format(book['가격'])))
             print('\n>> 정보수정이 정상 처리되었습니다. <<')
         else:
-            print('# 변경을 취소합니다.')
+            print('\n>> 변경을 취소합니다. <<')
         book['총액'] = book['가격'] * book['수량']
     else:
         print('\n!! 존재하지 않는 도서입니다. !!')
@@ -385,21 +475,59 @@ if __name__ == '__main__':
                     elif book_info == 5:
                         delete_book()
                     elif book_info == 6:
-                        exit_program()
+                        print('>>> !! 로그아웃 !!')
+                        break
                     else:
-                        print('!! 메뉴를 잘못 입력했습니다. !!')
+                        print('>>> !! 메뉴를 잘못 입력했습니다. !!')
 
                     input('\n>> 메뉴화면으로 돌아가시려면 Enter를 누르세요.')
 
-        # 리스트에 정보 없을때 회원가입 시키는거
         elif menu == 3:
+            
+
+            is_all_users_info = print_all_users()
+
+            while True:
+                    show_users_info()
+                    trans_info = int(input('\n>>> '))
+                    
+                    if trans_info == 1:
+                        modify_info()
+                    elif trans_info == 2:
+                        delete_info()
+                    elif trans_info == 3:
+                        break
+                    else:
+                        print('>>> !! 메뉴를 잘못 입력했습니다. !!')
+
+            '''
+            if is_all_users_info:
+                load_user_list()
+
+                while True:
+                    show_users_info()
+                    trans_info = int(input('\n>>> '))
+                    
+                    if trans_info == 1:
+                        modify_info()
+                    elif trans_info == 2:
+                        delete_info()
+                    elif trans_info == 3:
+                        exit_program
+                    else:
+                        print('!! 메뉴를 잘못 입력했습니다. !!')
+                    input('\n>> 메뉴화면으로 돌아가시려면 Enter를 누르세요.')
+            '''
+
+        elif menu == 4:
             exit_program()
+        
+        
         else:
-            print('!! 메뉴를 잘못 입력했습니다. !!')
+            print('>>> !! 메뉴를 잘못 입력했습니다. !!')
 
         input('\n>> 메뉴화면으로 돌아가시려면 Enter를 누르세요.')
         
             
 
     
-
